175e2adc049f23977c2217e5e74094d2ecbd253e
vahidabbasov <tu7hl7kxu@code.edu.az>
2022-12-20 19:35:21 +0400

    added counter task

--- src/App.js
+++ src/App.js
@@ -33,6 +33,38 @@ function App() {
     {/* <Button variant="contained" color="success" onClick={doGreen}>Green</Button>
     <Button variant="contained" color="error" style={{marginLeft: '58px'}} onClick={doRed}>Red</Button> */}
     <Button variant="contained"  style={{marginLeft: '58px'}} onClick={()=>{handleClick()}}>Toggle Color</Button>
+
+
+
+
+
+    {/* let [count, setCount] = useState(0)
+  const decrease = ()=>{
+    if (count>0) {
+      
+      setCount(--count);
+    }
+  }
+  const increase = ()=>{
+
+    setCount(++count);
+    
+  }
+  if (count>0) {
+    
+  }
+  
+  return (
+    
+    <div>
+      <Button color={count===0 ? "error" : "success"} variant="contained" onClick={decrease}>Decrease</Button>
+     <span>{count} {count===0 ? ":(" : ":)"}</span>
+     <Button variant="contained" onClick={increase}>Increase</Button>
+    </div>
+    
+  ); */}
+
+
     </div>
   )
 }
