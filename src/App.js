9ea980592ad30662ce5cb54bbcba82d0f4dea3c6
vahidabbasov <tu7hl7kxu@code.edu.az>
2022-12-20 19:33:35 +0400

    done toggle task

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
+import { Button } from '@mui/material';
+import { Box } from '@mui/system';
+import { useState } from 'react';
 import './App.css';
 
-import Welcome from './components/Welcome';
 
-
-let users = [{id:1, name:"Vahid", age:"20", src:"https://media.cnn.com/api/v1/images/stellar/prod/221218184732-messi-wc-trophy.jpg?c=16x9&q=h_270,w_480,c_fill"}, {id:2, name:"Cavidan", age:"19", src:"https://c.ndtvimg.com/2022-12/edffec18_lionel-messi_625x300_18_December_22.jpg?im=FaceCrop,algorithm=dnn,width=1200,height=675"}, {id:3, name:"Roman", age:"20", src:"https://i.guim.co.uk/img/media/fdc6b8c8fca6dbd090f7cd64db60d674a968d5ee/0_70_3018_1811/master/3018.jpg?width=465&quality=85&dpr=1&s=none"}];
 
 
 function App() {
+  let [active, setActive] = useState(false)
+
+  const handleClick = ()=>{
+    setActive(!active)
+  }
+  
+  
   return (
     <div>
-     {users.map((user)=>{
-      return <Welcome name={user.name} age ={user.age} src={user.src}/>
-     })}
+
+      
+
+      <Box
+      sx={{
+        width: 300,
+        height: 300,
+        backgroundColor: active ? "red" : "green",
+        '&:hover': {
+          backgroundColor: 'primary.main',
+          opacity: [0.9, 0.8, 0.7],
+        },
+      }}
+    />
+    {/* <Button variant="contained" color="success" onClick={doGreen}>Green</Button>
+    <Button variant="contained" color="error" style={{marginLeft: '58px'}} onClick={doRed}>Red</Button> */}
+    <Button variant="contained"  style={{marginLeft: '58px'}} onClick={()=>{handleClick()}}>Toggle Color</Button>
     </div>
-    
-  );
+  )
 }
 
 export default App;
--- src/components/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Navbar() {
-  return (
-    <div> this is Navbar</div>
-  )
-}
-
-export default Navbar
